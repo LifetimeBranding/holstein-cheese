ac62854676e6ffda0bd62059fbf4633db907ae3b
Phil Fevry <pf@phlfvry.com>
2017-10-08 15:32:13 -0400

    styled 404 page

--- resources/templates/404.php
+++ resources/templates/404.php
@@ -1,3 +1,6 @@
-<?php 
-  echo '404';
-?>
+<div class="row hero mx-auto" id="hero-404page">
+    <div class="hero-content-container py-5">
+      <h1 class="text-bold text-uppercase mt-3">Page Not Found</h1>
+      <hr class="divider-header">
+    </div>
+  </div>
